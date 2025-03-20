057c77c89cd080a955bc52401a7547eac77f8df0
anassohail99 <anas.sohailabs@gmail.com>
2025-03-21 03:01:05 +0500

    sys vars

---
+++ sysvar/tests/sysvar.ts
@@ -0,0 +1,15 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Sysvar } from "../target/types/sysvar";
+
+describe("sysvar", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Sysvar as Program<Sysvar>;
+
+  it("Get day of the week", async () => {
+    const tx = await program.methods.getDayOfTheWeek().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
